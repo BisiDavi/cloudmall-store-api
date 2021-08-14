799313a92cf709a4edbfac5dd6341717bbaf17d7
BisiDavi <oludavidconnect@gmail.com>
2021-08-14 19:46:28 +0100

    auth controller updated

---
+++ controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const bcrypt  = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const StoreAdmin = require('../models/storeAdmin.model');
+
+const login = async (req,res) => {
+    try{
+        const {email, password} = req.body;
+
+        let result ;
+        await StoreAdmin.findOne({email}, (err, storeAdmin) => {
+            if(err){
+                result.error = `User doesn't exist, please register as an admin, ${err}`
+             return  res.send(result)
+            }
+            bcrypt.compare(password, storeAdmin.password).then((match) => {
+                if(match){
+                    const payload  = {
+                        email: storeAdmin.email,
+                        role:"admin"
+                    }
+                    const options =  {
+                        expiresIn: "30d",
+                        issuer: "https://cloudmall.africa"
+                    }
+                    const secret = process.env.JWT_SECRET;
+                    const token =  jwt.sign(payload, secret, options);
+                    result.token = token;
+                    result.message = "logged in as an admin";
+                    result.data = storeAdmin;
+                    return res.send(result);
+                }
+            })
+        })
+        
+
+    }catch(error){
+        console.error('error', error);
+        res.send(error);
+    }
+}
+
+module.exports = login;
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const validateToken = require('../utils/validateToken').validateToken;
+

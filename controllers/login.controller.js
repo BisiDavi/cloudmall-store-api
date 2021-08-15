0fd2bf6303489e6ab8222a7db5b3142feb54c337
BisiDavi <oludavidconnect@gmail.com>
2021-08-15 15:47:37 +0100

    login controller updated

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -29,7 +29,7 @@ exports.login = async (req,res) => {
                     result.data = storeAdmin;
                     return res.send(result);
                 }else{
-                    return res.send({err, message:"wrong credentials"})
+                    return res.status(400).send({err, message:"wrong credentials !"})
                 }
             })
         })        

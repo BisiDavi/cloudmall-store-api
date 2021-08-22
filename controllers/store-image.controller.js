a95a474e6ad1e1883acf0f0407b8eb1abaa450b8
BisiDavi <oludavidconnect@gmail.com>
2021-08-22 10:29:25 +0100

    image upload enabled

---
+++ controllers/store-image.controller.js
@@ -0,0 +1,34 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+exports.uploadStoreImage = () => {
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: "CLOUDMALL_STORES_IMAGE",
+      public_id: (req, file) => {
+        console.log("image req", req);
+        console.log("image file", file);
+        `storeImage-${file}`;
+      },
+    },
+  });
+
+  const upload = multer({ storage: storage });
+  return upload;
+};
+
+//   _app.post(
+//     "/",
+//     upload.single("picture", async (req, res) => {
+//       StoreAdmin.create({
+//         image: req.file.path,
+//       });
+//     })
+//   );

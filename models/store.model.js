2f7044c9dfe07b9f0deddd00eedc8027b9ba3bf1
BisiDavi <oludavidconnect@gmail.com>
2021-08-29 15:37:26 +0100

    store model updated

--- models/store.model.js
+++ models/store.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StoreSchema = new Schema({
-    name:String,
+    storeName:String,
     storeEmail:String,
     phoneNumber:String,
-    address:String,
+    storeAddress:String,
     storeType:String,
     openingDays:String,
     storeImage:String,   
